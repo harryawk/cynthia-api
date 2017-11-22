5578021fd4fdf797a6e34a896a4625192aa79c0f
Harry Alvin Waidan Kefas <harry.kambey@gmail.com>
2017-11-23 00:45:59 +0700

    init commit

---
+++ index.js
@@ -0,0 +1,23 @@
+var express = require('express')
+var bodyParser = require('body-parser')
+var the_request = require('request')
+
+var app = express().use(bodyParser.json()); // creates express http server
+
+// Sets server port and logs message on success
+app.listen(process.env.PORT || 1337, () => console.log('webhook is listening'));
+
+app.get('/', (req, res) => {
+  response.json({
+    try: "Coba API",
+    request: "data",
+    data: "the_data"
+  })
+})
+
+app.post('/postrequest', (req, res) => {
+  response.json({
+    test: 'POST REQUEST',
+    request: 'POST REQ'
+  })
+})
