a669bdef9ca0a283796f3b0a2763313c357e30b6
Harry Alvin Waidan Kefas <harry.kambey@gmail.com>
2017-11-23 11:32:11 +0700

    init commit

--- index.js
+++ index.js
@@ -92,15 +92,15 @@ app.post('/postrequest', (req, res) => {
       console.log(result.result[0])
       console.log('0')
       // if (x == result.result.length - 1) 
-      fs.writeFile('./public/1.jpg', 'data:image/jpeg;base64,' + result.result[1], 'binary', function(err) {
-        if (err) console.error(err)
-        console.log('File saved')
-        results.push('http://f072a7ca.ngrok.io/static/' + '1' + '.jpg')
-        console.log('1')
-        // if (x == result.result.length - 1) 
-        res.json(results)
-      })
     })
+    fs.writeFile('./public/1.jpg', 'data:image/jpeg;base64,' + result.result[1], 'binary', function(err) {
+      if (err) console.error(err)
+      console.log('File saved')
+      results.push('http://f072a7ca.ngrok.io/static/' + '1' + '.jpg')
+      console.log('1')
+      // if (x == result.result.length - 1) 
+    })
+    res.json(results)
     // for (var x = 0; x < result.result.length; x++) {
     //   // res.json(results)
     //   // results.push('data:image/jpeg;base64,' + result.result[x])
